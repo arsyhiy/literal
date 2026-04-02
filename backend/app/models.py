e5a3a26da6248dc06a691677accd5830a4040894
arsyhiy <arsyhiy32@gmail.com>
2026-04-02 19:54:33 +0300

    refactor models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,10 +5,8 @@ from django.db import models
 
 class author(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(
-        max_length=255, default="anonymous"
-    )  # было решено оставить так как не вижу смысла делать несколько полей может конечно будет причина пока невижу.
-    image = models.ImageField(blank=True)  #  надо написать дефолтный вариант
+    name = models.CharField(max_length=255, null=False, blank=False)
+    image = models.ImageField(null=True, blank=True, default="images/default.png")
 
     def __str__(self):
         return self.name
@@ -16,15 +14,15 @@ class author(models.Model):
 
 class publisher(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, default="noname")
-    image = models.ImageField(blank=True)  #  надо написать дефолтный вариант
+    name = models.CharField(max_length=255, null=False, blank=False)
+    image = models.ImageField(null=True, blank=True, default="images/default.png")
 
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, default="noname")
+    name = models.CharField(max_length=255, null=False, blank=False)
     parent = models.ForeignKey(
         "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
     )
@@ -35,15 +33,12 @@ class Category(models.Model):
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, default="unknown")
+    name = models.CharField(max_length=255, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField(null=False, blank=False)
-    description = models.TextField(
-        null=False,
-        blank=False,
-        max_length=1000,  # решил оставить один милион как максимальное значение
-    )
-    image = models.ImageField(blank=True)  #  надо написать дефолтный вариант
+    description = models.TextField(max_length=1000, default="no description")
+    image = models.ImageField(null=True, blank=True, default="images/default.png")
+
     weight = models.DecimalField(
         max_digits=1000, decimal_places=2, null=False, blank=False
     )
@@ -60,17 +55,13 @@ class Product(models.Model):
 
 class Book(Product):
     author = models.ForeignKey(author, on_delete=models.CASCADE, related_name="author")
-    isbn = models.CharField(
-        max_length=20, blank=False
-    )  # честно представления не имею какая должна быть длина isbn
+    isbn = models.CharField(max_length=20, null=False, blank=False)
 
     coverType = models.CharField(max_length=100, null=False, blank=False)
-    numberOfPages = models.IntegerField(default=1)
+    numberOfPages = models.IntegerField(null=False, blank=False)
 
 
 class BoardGame(Product):
-    players = models.CharField(
-        max_length=255
-    )  # кто его знает сколько может играть в одну игру множество людей
+    players = models.CharField(max_length=255)
 
-    timeOfPlaying = models.IntegerField(default=1)
+    timeOfPlaying = models.IntegerField(null=False, blank=False)
